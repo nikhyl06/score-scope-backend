1161b29174938cd87f7df1c1457371feaeb8e469
nikhyl06 <nikhilshankhwar2003@gmail.com>
2025-03-01 23:57:21 +0530

    configured urls for frontend

--- server.js
+++ server.js
@@ -10,8 +10,15 @@ const port = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
-// Middleware
-app.use(cors());
+
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://score-scope-frontend.vercel.app"
+        : "http://localhost:5173",
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
